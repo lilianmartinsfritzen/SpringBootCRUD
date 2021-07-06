993d9150bd66f43977cee46dc9c7555af6e0dc03
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-07-05 22:48:05 -0300

    Spring Boot CRUD project start

---
+++ backend/src/main/java/com/lilianmfritzen/activitycrud/ActivitycrudApplication.java
@@ -0,0 +1,13 @@
+package com.lilianmfritzen.activitycrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitycrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitycrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lilianmfritzen/activitycrud/ActivitycrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilianmfritzen.activitycrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitycrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
