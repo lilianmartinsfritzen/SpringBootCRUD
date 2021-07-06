dfb5a8a332f6a75ad35ffd98395183d5bc89d608
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-07-06 15:17:03 -0300

    DTO

--- backend/src/main/java/com/lilianmfritzen/activitycrud/services/ClientService.java
+++ backend/src/main/java/com/lilianmfritzen/activitycrud/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.lilianmfritzen.activitycrud.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.lilianmfritzen.activitycrud.dto.ClientDTO;
 import com.lilianmfritzen.activitycrud.entities.Client;
 import com.lilianmfritzen.activitycrud.repositories.ClientRepository;
 
@@ -14,7 +17,9 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 	
-	public List<Client> findAll() {
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<ClientDTO> findAll() {
+		List<Client> list =  repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 	}
 }
