4d75d26b6dabd575bfefa3e73024ff77e39f24cc
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-07-07 22:33:30 -0300

    Update client

--- backend/src/main/java/com/lilianmfritzen/activitycrud/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/lilianmfritzen/activitycrud/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.lilianmfritzen.activitycrud.services.exceptions.EntityNotFoundException;
+import com.lilianmfritzen.activitycrud.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException mistake, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException mistake, HttpServletRequest request) {
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
