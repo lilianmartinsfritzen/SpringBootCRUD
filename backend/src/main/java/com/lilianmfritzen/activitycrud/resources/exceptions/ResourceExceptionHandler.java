396aa19604a7ed267e27487a158b0e380c164171
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-07-07 21:02:10 -0300

    Exception handling

---
+++ backend/src/main/java/com/lilianmfritzen/activitycrud/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.lilianmfritzen.activitycrud.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lilianmfritzen.activitycrud.services.exceptions.EntityNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException mistake, HttpServletRequest request) {
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Resource not found");
+		err.setMessage(mistake.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+}
