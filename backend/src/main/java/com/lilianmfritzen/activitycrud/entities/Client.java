82583c103cce4739860978987fb8de9932245b68
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-07-07 21:52:23 -0300

    POST new Client

--- backend/src/main/java/com/lilianmfritzen/activitycrud/entities/Client.java
+++ backend/src/main/java/com/lilianmfritzen/activitycrud/entities/Client.java
@@ -21,10 +21,11 @@ public class Client implements Serializable {
 	private String name;
 	private String cpf;
 	private Double income;
-	private Integer children;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant birthDate;
+	
+	private Integer children;
 	
 	public Client() {
 	}
